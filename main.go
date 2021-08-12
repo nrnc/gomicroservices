48a69e62cab1f5d6bdbcce9e63067f02f0f7c817
Narendra Chukka <narendra.chukka@yahoo.in>
2021-08-12 14:49:18 +0530

    second lecture

--- main.go
+++ main.go
@@ -1,21 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.comn/nchukkaio/gomicroservices/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	// http.ListenAndServe(":8080", sm)
+
+	s := &http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
+			l.Fatal(err)
 		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-	http.ListenAndServe(":8080", nil)
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	// signal.Notify(sigChan, os.Kill)
+	sig := <-sigChan
+
+	l.Println("Recieved terminate,graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(tc)
 }
